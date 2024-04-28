1df03e601f216bd5afdfee2327dcb08781a499eb
Agustin Chiarotto <agusgc_99@hotmail.com>
2024-04-28 19:02:14 -0300

    correcciones

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,7 @@
 import TasksContainer from "../../components/TasksContainer/TasksContainer";
 
-
-export default function Home(){
-  return (
-    <TasksContainer/>
-  )
+//Muy buen trabajo.
+//Unico detalle: El task container no deberia existir, si se fijan esta pagina quedo vacia. Lo mejor hubiera sido tener todo lo de ese componente aca y obviarlo.
+export default function Home() {
+  return <TasksContainer />;
 }
